b6ec94ba72d2c86bf67527a48230b08f01b954fc
Haoyu Zhang <haoyuzhang@google.com>
2019-04-25 15:39:01 -0700

    Fix cron job

--- cron-continuous-build.sh
+++ cron-continuous-build.sh
@@ -7,8 +7,7 @@ date_time=$(date '+%Y-%m-%d-%H-%M-%S')
 tmp_log_file="/tmp/continuous-build.log"
 gs_log_file="${GCS_LOG_PATH}/build-${date_time}.log"
 
-pid=$(pgrep -f cron-continuous-build.sh)
-if $pid > /dev/null; then
+if $(pgrep -f cron-continuous-build.sh > /dev/null); then
   echo "Last cron job still running. pid=$pid" > /tmp/dummy.log
   gsutil cp /tmp/dummy.log ${gs_log_file}
   exit 1
